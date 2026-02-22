c5be910cb079a098e0bd4d6f7e30a4a170158f28
𝐍𝐲𝐱𝐞𝐧 <jgarland13191@gmail.com>
2026-02-22 19:43:44 +0000

    Added better validation and fixed image issues in roleplay cog

--- cogs/role_play.py
+++ cogs/role_play.py
@@ -9,15 +9,17 @@ class RolePlayCog(commands.Cog):
     # !kill command
     @commands.command(name="kill")
     async def kill(self, ctx, member: discord.Member = None):
-        '''Kill another user. (not really)'''
+        """Kill another user. (not really)"""
         try:
-            target = member or ctx.author
+            if not member:
+                return await ctx.send("You need to mention someone to use this command on them!")
+            target = member
             kill_gifs = [
-                "https://tenor.com/bRHng.gif"
+                "https://i.pinimg.com/originals/36/d5/fd/36d5fd46d8331661819031b2b7adcda4.gif"
             ]
             kill_embed = discord.Embed(title="Kill", description=f"{ctx.author.display_name} killed {target.display_name}!", color=discord.Color.red())
             kill_embed.set_image(url=random.choice(kill_gifs))
-            kill_embed.set_footer(text="This is a joke, don't actually kill anyone.")
+            kill_embed.set_footer(text="*This is a joke, don't actually kill anyone.*")
             await ctx.send(embed=kill_embed)
         except Exception as e:
             error_embed = discord.Embed(title="Error", description=f"An error occurred:\n```\n{e}\n```", color=discord.Color.red())
@@ -26,39 +28,29 @@ class RolePlayCog(commands.Cog):
     # !fuck command
     @commands.command(name="fuck")
     async def fuck(self, ctx, member: discord.Member = None):
-        '''Fuck another user. (not really)'''
-        target = member or ctx.author
+        """Fuck another user. (not really)"""
+        if not member:
+            return await ctx.send("You need to mention someone to use this command on them!")
+        target = member
         await ctx.send(f"{ctx.author.display_name} fucked {target.display_name}!")
 
     # !hug command
     @commands.command(name="hug")
     async def hug(self, ctx, member: discord.Member = None):
-        '''Hug another user. (not really)'''
+        """Hug another user. (not really)"""
+        if not member:
+            return await ctx.send("You need to mention someone to use this command on them!")
         target = member
-        if target is None:
-            await ctx.send("You need to mention someone to hug them!")
-        else:
-            await ctx.send(f"{ctx.author.display_name} hugged {target.display_name}! :hugging:")
+        await ctx.send(f"{ctx.author.display_name} hugged {target.display_name}! :hugging:")
 
     # !makeout command
     @commands.command(name="makeout")
     async def makeout(self, ctx, member: discord.Member = None):
-        '''Make out with another user. (not really)'''
+        """Make out with another user. (not really)"""
+        if not member:
+            return await ctx.send("You need to mention someone to use this command on them!")
         target = member
-        if target is None:
-            await ctx.send("You need to mention someone to make out with them!")
-        else:
-            await ctx.send(f"{ctx.author.display_name} made out with {target.display_name}! :kissing_heart:")
-
-    # !rape command
-    @commands.command(name="rape")
-    async def rape(self, ctx, member: discord.Member = None):
-        '''Rape another user. (not really)'''
-        target = member
-        if target is None:
-            await ctx.send("You have to mention someone to use this command on them!")
-        else:
-            await ctx.send(f"{ctx.author.display_name} raped {target.display_name}!")
+        await ctx.send(f"{ctx.author.display_name} made out with {target.display_name}! :kissing_heart:")
 
 
 async def setup(bot):
