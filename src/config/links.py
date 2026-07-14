ffb513ab4d6e109ae3f3564a106dafd768bd8e42
Nyxen <developer51709@proton.me>
2026-07-13 21:16:59 -0400

    Fixed the help commands community server link

--- src/config/links.py
+++ src/config/links.py
@@ -1,6 +1,6 @@
 # this is where the bots links can be updated
 
-COMMUNITY_SERVER = "https://discord.gg/aether-lounge"
+COMMUNITY_SERVER = "https://discord.gg/3sAKtw3dMa"
 SUPPORT_SERVER = "https://discord.gg/3JFbm2wfNk"
 GITHUB = "https://github.com/developer51709/Niko"
 WEBSITE = "https://developer51709.github.io/Niko"
